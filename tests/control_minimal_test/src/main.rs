adda6ab92bb7b76de5d70c8d59ef6ec2c15d85a2
Adrien Drapala-Bizouarn <adrien@eatcala.com>
2023-11-04 23:36:14 +0000

    tests: Add tests for minimal control

---
+++ tests/control_minimal_test/src/main.rs
@@ -0,0 +1,14 @@
+use std::f32::consts::PI;
+
+fn main() {
+    println!("Hello, world!");
+
+    // let variable_0: usize = controller_omniwheel_3::add(199, 1);
+    // println!("{}", variable_0);
+
+    // let variable_1: (f32, f32, f32) = controller_omniwheel_3::convert(PI/2.0, 1.0, 0.0);
+    // println!("variable_1: {:?}", variable_1);
+
+    let variable_2: (f32, f32, f32) = control_minimal::convert(PI/2.0, 0.9, 0.0);
+    println!("variable_2: {:?}", variable_2);
+}
