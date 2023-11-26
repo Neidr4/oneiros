f286c4cafbc44feb515f63c609033fbd653f2792
neidr4 <adrien@eatcala.com>
2023-11-26 16:19:10 +0100

    feat: Add other channels to PWM

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # oneiros
 3 legged dog
+
+## Requirements
+
+Must contain in `/boot/config.txt`:
+```txt
+dtparam=pwm
+dtoverlay=pwm-2chan,pin=18,func=2,pin2=19,func2=2
+```
--- control_raspberry_adapter/src/lib.rs
+++ control_raspberry_adapter/src/lib.rs
@@ -8,7 +8,8 @@ use rppal::gpio::{Gpio, OutputPin};
 use rppal::pwm::{Channel, Pwm, Polarity};
 
 // TODO: Add a function start sending in a thread
-const PWM_FREQ_MIN: u32 = 400;
+const PWM_FREQ_MIN: u32 = 1;
+const GPIO_PWM: u8 = 23;
 static EXIT_EVENT: AtomicBool = AtomicBool::new(false);
 
 pub fn setup_io() {
@@ -26,16 +27,22 @@ fn is_setup_io() -> bool {
     return result
 }
 
-// fn convert_to_pwm(motor_speeds: &[f32; 3]) {
 fn convert_to_pwm(motor_speeds: &[f32; 3]) -> Result<(), Box<dyn Error>> {
     let pwm_0 = Pwm::with_frequency(Channel::Pwm0, 2.0, 0.25, Polarity::Normal, true)?;
+    let pwm_1 = Pwm::with_frequency(Channel::Pwm1, 2.0, 0.25, Polarity::Normal, true)?;
+    let mut pwm_2 = Gpio::new()?.get(GPIO_PWM)?.into_output();
+    let _ = pwm_2.set_pwm_frequency(2.0, 0.25);
+    let mut pwms_previous: [f32; 3] = [0.0; 3];
     loop {
-        // pwm_0.set_frequency(8.0, 0.5)?;
-        // println!("{:?}", motor_speeds);
-        if EXIT_EVENT.load(Ordering::Relaxed) {
-            break;
+        if EXIT_EVENT.load(Ordering::Relaxed) { break; }
+        if *motor_speeds != pwms_previous {
+            println!("{:?}", motor_speeds);
+            pwm_0.set_frequency((motor_speeds[0] * PWM_FREQ_MIN as f32).into(), 0.5)?;
+            pwm_1.set_frequency((motor_speeds[1] * PWM_FREQ_MIN as f32).into(), 0.5)?;
+            pwm_2.set_pwm_frequency((motor_speeds[2] * PWM_FREQ_MIN as f32).into(), 0.5)?;
+            pwms_previous = motor_speeds.clone();
         }
-        thread::sleep(Duration::from_millis(1));
+        thread::sleep(Duration::from_millis(100));
     }
     return Ok(());
 }
@@ -45,11 +52,12 @@ pub fn start_sending_to_io(motor_pwms: &'static [f32; 3]) -> Result<(), Box<dyn 
         println!("I/O have not been setup\n Please setup I/O before converting.");
         return Ok(());
     }
+    // TODO: verify that the values are legal
     thread::spawn(move || {
         println!("Starting the PWM thread");
         EXIT_EVENT.store(false, Ordering::Relaxed);
         let _ = convert_to_pwm(&motor_pwms);
-        println!("Leaving the PWM thread");
+        // println!("Leaving the PWM thread");
     });
     return Ok(());
 }
