21182e75216214a98c441c5e334a63f530eb5d92
Neidr4 <a.drapbiz@gmail.com>
2023-11-28 22:02:57 +0000

    feat: Refactor the raspberry adaptor class to not have the pwm as attributes

--- control_raspberry_adapter/src/lib.rs
+++ control_raspberry_adapter/src/lib.rs
@@ -7,79 +7,70 @@ use std::time::Duration;
 use rppal::gpio::{Gpio, OutputPin};
 use rppal::pwm::{Channel, Pwm, Polarity};
 
-// TODO: Add a function start sending in a thread
 const PWM_FREQ_MIN: u32 = 1;
 const GPIO_PWM: u8 = 23;
 static EXIT_EVENT: AtomicBool = AtomicBool::new(false);
 
-static RASP_ADAPT: RaspberryAdapter = RaspberryAdapter();
+// static RASP_ADAPT: RaspberryAdapter = RaspberryAdapter();
+static RASP_ADAPT: RaspberryAdapter = RaspberryAdapter {
+    exit_event: true,
+    pwm_0: Pwm::new(Channel::Pwm0),
+    pwm_1: Pwm::new(Channel::Pwm1),
+    pwm_2: Gpio::new()?.get(GPIO_PWM)?.into_output(),
+    pwm_min_freq: PWM_FREQ_MIN,
+    speed_desired: [0.0; 3],
+    speed_current: [0.0; 3],
+
+};
 
 struct RaspberryAdapter {
     exit_event: bool,
-    pwm_0: Pwm,
-    pwm_1: Pwm,
-    pwm_2: OutputPin,
     pwm_min_freq: u32,
+    dir_pins: [u8; 3],
     gpio_pwm: u8,
+    dir_desired: [f32; 3],
+    dir_current: [f32; 3],
     speed_desired: [f32; 3],
     speed_current: [f32; 3],
 }
 
-impl RaspberryAdapter {
-    fn new(&self) -> Self {
-        Self {
-            setup(),
-            speed_current = [0.0; 3],
-        }
-    }
 
+impl RaspberryAdapter {
+
+    // TODO: change this to setup
     pub fn start_sending_to_io(&self, motor_pwms: &'static [f32; 3]) -> Result<(), Box<dyn Error>> {
-        // TODO: verify that the values are legal
         thread::spawn(move || {
             println!("Starting the PWM thread");
             EXIT_EVENT.store(false, Ordering::Relaxed);
-            let _ = self.set_pwm();
+            let _ = self.run_();
             // println!("Leaving the PWM thread");
         });
         return Ok(());
     }
 
-    fn setup(&self) -> Result<(), Box<dyn Error>> {
-        self.pwm_0 = Pwm::with_frequency(Channel::Pwm0, 2.0, 0.25, Polarity::Normal, true)?;
-        self.pwm_1 = Pwm::with_frequency(Channel::Pwm1, 2.0, 0.25, Polarity::Normal, true)?;
-        self.pwm_2 = Gpio::new()?.get(GPIO_PWM)?.into_output();
-        let _ = self.pwm_2.set_pwm_frequency(2.0, 0.25);
-        self.speed_current = [0.0; 3];
-        Ok(())
+    pub fn update_speed_value(&self, motor_pwms: [f32; 3]) {
+        // TODO: Verify that the values are legal
+        // TODO: extract the minus for dir
+        self.speed_desired = self.compute_dir();
     }
 
-    fn set_pwm(&self) -> Result<(), Box<dyn Error>>  {
-        loop {
-            self.pwm_0.set_frequency((self.speed_desired[0] * PWM_FREQ_MIN as f32).into(), 0.5)?;
-            self.pwm_1.set_frequency((self.speed_desired[1] * PWM_FREQ_MIN as f32).into(), 0.5)?;
-            self.pwm_2.set_pwm_frequency((self.speed_desired[2] * PWM_FREQ_MIN as f32).into(), 0.5)?;
-            if self.exit_event == false {
-                break;
-            }
-        }
-        self.pwm_0.disable();
-        self.pwm_1.disable();
-        self.pwm_2.clear_pwm();
-        Ok(())
-    }
-
-    fn run_pwm(&self) {
-        loop {
-            if EXIT_EVENT.load(Ordering::Relaxed) { break; }
-            if *motor_speeds != pwms_previous {
-                println!("{:?}", motor_speeds);
-                self.pwm_0.set_frequency((motor_speeds[0] * PWM_FREQ_MIN as f32).into(), 0.5)?;
-                self.pwm_1.set_frequency((motor_speeds[1] * PWM_FREQ_MIN as f32).into(), 0.5)?;
-                self.pwm_2.set_pwm_frequency((motor_speeds[2] * PWM_FREQ_MIN as f32).into(), 0.5)?;
-                self.pwms_previous = motor_speeds.clone();
-            }
+    fn run_pwm(&self) -> Result<(), Box<dyn Error>>  {
+        // TODO: instanciate the pwms
+        // let pwm_0 = Pwm::with_frequency((self.speed_desired[0] * PWM_FREQ_MIN as f32).into(), 0.5)?;
+        // let pwm_1 = Pwm::with_frequency((self.speed_desired[1] * PWM_FREQ_MIN as f32).into(), 0.5)?;
+        // let mut pwm_2 = Gpio::.into(), 0.5)?;
+        loop{
+            // TODO: update the PWM depending on pwm desired
+            pwm_0.set_frequency((self.speed_desired[0] * PWM_FREQ_MIN as f32).into(), 0.5)?;
+            pwm_1.set_frequency((self.speed_desired[1] * PWM_FREQ_MIN as f32).into(), 0.5)?;
+            pwm_2.set_pwm_frequency((self.speed_desired[2] * PWM_FREQ_MIN as f32).into(), 0.5)?;
+            if self.exit_event == false {break;}
             thread::sleep(Duration::from_millis(100));
         }
+        pwm_0.disable();
+        pwm_1.disable();
+        pwm_2.clear_pwm();
+        Ok(())
     }
 
     pub fn stop_sending_to_io(&self) {
@@ -88,23 +79,4 @@ impl RaspberryAdapter {
         self.exit_event = true;
     }
 }
-
-pub fn update_speed(speeds: [f32; 3]) {
-
-}
-fn convert_to_pwm(motor_speeds: &[f32; 3]) -> Result<(), Box<dyn Error>> {
-    loop {
-        if EXIT_EVENT.load(Ordering::Relaxed) { break; }
-        if *motor_speeds != pwms_previous {
-            println!("{:?}", motor_speeds);
-            pwm_0.set_frequency((motor_speeds[0] * PWM_FREQ_MIN as f32).into(), 0.5)?;
-            pwm_1.set_frequency((motor_speeds[1] * PWM_FREQ_MIN as f32).into(), 0.5)?;
-            pwm_2.set_pwm_frequency((motor_speeds[2] * PWM_FREQ_MIN as f32).into(), 0.5)?;
-            pwms_previous = motor_speeds.clone();
-        }
-        thread::sleep(Duration::from_millis(100));
-    }
-    return Ok(());
-}
-
 
