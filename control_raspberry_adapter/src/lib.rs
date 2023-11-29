451e5764dd5ab97c3a86c3288d7b416d0c0ffa2e
Neidr4 <a.drapbiz@gmail.com>
2023-11-29 21:13:43 +0000

    feat: Create proper structure for threads of pwm and dir. Problems over referencing

--- control_raspberry_adapter/src/lib.rs
+++ control_raspberry_adapter/src/lib.rs
@@ -12,22 +12,19 @@ const GPIO_PWM: u8 = 23;
 const GPIO_DIR0: u8 = 24;
 const GPIO_DIR1: u8 = 25;
 const GPIO_DIR2: u8 = 26;
+const THREAD_SLEEP: u8 = 100;
 static EXIT_EVENT: AtomicBool = AtomicBool::new(false);
 
 // static RASP_ADAPT: RaspberryAdapter = RaspberryAdapter();
 static RASP_ADAPT: RaspberryAdapter = RaspberryAdapter {
-    exit_event: EXIT_EVENT,
+    exit_event: true,
     pwm_min_freq: PWM_FREQ_MIN,
     speed_desired: [0.0; 3],
-    speed_current: [0.0; 3],
-
 };
 
 struct RaspberryAdapter {
     exit_event: bool,
     pwm_min_freq: u32,
-    dir_pins: [u8; 3],
-    gpio_pwm: u8,
     speed_desired: [f32; 3],
 }
 
@@ -39,9 +36,12 @@ impl RaspberryAdapter {
             println!("Starting the PWM thread");
             EXIT_EVENT.store(false, Ordering::Relaxed);
             let _ = self.run_pwm();
-            // println!("Leaving the PWM thread");
         });
-        // TODO: Do the same for dir
+        thread::spawn(move || {
+            println!("Starting the DIR thread");
+            EXIT_EVENT.store(false, Ordering::Relaxed);
+            let _ = self.run_dir();
+        });
         return Ok(());
     }
 
@@ -49,23 +49,26 @@ impl RaspberryAdapter {
     pub fn update_speed_value(&self, motor_pwms: [f32; 3]) {
         // TODO: Make sure the threads are started before using this method
         // TODO: Verify that the values are legal
-        // TODO: extract the minus for dir
-        self.speed_desired = motor_pwms;
+        self.speed_desired = motor_pwms.clone();
     }
 
     fn run_pwm(&self) -> Result<(), Box<dyn Error>>  {
         let pwm_0: Pwm = Pwm::new(Channel::Pwm0)?;
         let pwm_1: Pwm = Pwm::new(Channel::Pwm1)?;
         let mut pwm_2: OutputPin = Gpio::new()?.get(GPIO_PWM)?.into_output();
-        
+        let mut speed_previous: [f32; 3] = [0.0; 3];
         loop{
-            // TODO: add a check to see if the values have changed (this can be local)
+            // Checking if anything has changed
+            if self.exit_event == false {break;}
+            thread::sleep(Duration::from_millis(THREAD_SLEEP.into()));
+            if self.speed_desired == speed_previous { continue; }
+            speed_previous = self.speed_desired.clone();
+            // Setting the frequency
             pwm_0.set_frequency((self.speed_desired[0] * PWM_FREQ_MIN as f32).into(), 0.5)?;
             pwm_1.set_frequency((self.speed_desired[1] * PWM_FREQ_MIN as f32).into(), 0.5)?;
             pwm_2.set_pwm_frequency((self.speed_desired[2] * PWM_FREQ_MIN as f32).into(), 0.5)?;
-            if self.exit_event == false {break;}
-            thread::sleep(Duration::from_millis(100));
         }
+        println!("Disabling the PWMs");
         pwm_0.disable();
         pwm_1.disable();
         pwm_2.clear_pwm();
@@ -76,16 +79,26 @@ impl RaspberryAdapter {
         let mut dir_0: OutputPin = Gpio::new()?.get(GPIO_DIR0)?.into_output();
         let mut dir_1: OutputPin = Gpio::new()?.get(GPIO_DIR1)?.into_output();
         let mut dir_2: OutputPin = Gpio::new()?.get(GPIO_DIR2)?.into_output();
+        let mut list_dir: [OutputPin; 3] = [dir_0, dir_1, dir_2];
         let mut dir_current: [bool; 3] = [true; 3];
         loop {
-            // TODO: add a check to see if the values have changed (this can be local)
-            let mut dir_desired = self.speed_desired.iter().map(|x| x.is_sign_positive()).collect();
-            if dir_current == dir_desired {
-                // TODO: iterate and assing to dir pin
-                dir_0.set_low();
+            if self.exit_event == false {break;}
+            for (index, &motor_speed) in self.speed_desired.iter().enumerate() {
+                // Checking if anything has changed
+                let state = motor_speed.is_sign_positive();
+                if state == dir_current[index] {continue};
+                dir_current[index] = !dir_current[index];
+                // Setting the direction pin
+                if state {
+                    list_dir[index].set_high();
+                } else {
+                    list_dir[index].set_low();
+                }
             }
+            thread::sleep(Duration::from_millis(THREAD_SLEEP.into()));
         }
-
+        println!("Quitting the DIR thread");
+        Ok(())
     }
 
     pub fn stop_sending_to_io(&self) {
