1b39a456e36c98255bae5e7a6639513dcaebf5e2
Neidr4 <a.drapbiz@gmail.com>
2023-12-12 20:22:04 +0000

    feat: Change the name of the keyboard teleoperation for a more explicit name

---
+++ tests/teleop_keyboard_test/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    println!("Testing teleop_keyboard");
+
+    teleop_keyboard::usage();
+
+    teleop_keyboard::teleoperate();
+
+}
--- tests/teleoperation_minimal_test/src/main.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    println!("Testing teleoperation_basic");
-
-    teleoperation_basic::usage();
-
-    teleoperation_basic::teleoperate();
-
-}
