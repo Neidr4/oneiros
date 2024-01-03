5f0441edc983cf9b2e11d29b183bd00f71c8a249
Neidr4 <a.drapbiz@gmail.com>
2024-01-03 22:12:15 +0000

    feat: Add some more binding keys. OnceLock doesn't work because it's made to be set only once :'( Should've read the description better)

--- teleop_keyboard/src/lib.rs
+++ teleop_keyboard/src/lib.rs
@@ -5,6 +5,7 @@ use std::{io, thread::sleep, time::Duration};
 use inputbot::KeybdKey::*;
 
 static COMMAND: OnceLock<[f32; 3]> = OnceLock::new();
+const SLEEP: u8 = 1;
 
 pub fn usage() {
     println!(" ------------------------- ");
@@ -33,31 +34,45 @@ fn teleoperate() {
     // TODO: Add SPACE for stop
     // TODO: Add ESC for stop
     // TODO: Set the sleep duration as a constant
+    QKey.bind(move || {
+        let _ = COMMAND.set([(3.0*PI/4.0), 0.0, 0.0]);
+        sleep(Duration::from_millis(SLEEP.into()));
+    });
+
     WKey.bind(move || {
         println!("W key has been pressed");
         let _ = COMMAND.set([PI/2.0, 0.0, 0.0]);
-        sleep(Duration::from_millis(100));
+        sleep(Duration::from_millis(SLEEP.into()));
     });
 
-    QKey.bind(move || {
-        callback("Q".to_string());
-        let _ = COMMAND.set([1.0, 0.0, 0.0]);
-        sleep(Duration::from_millis(100));
+    EKey.bind(move || {
+        let _ = COMMAND.set([(3.0*PI/4.0), 0.0, 0.0]);
+        sleep(Duration::from_millis(SLEEP.into()));
     });
 
     AKey.bind(move || {
-        callback("A".to_string());
-        sleep(Duration::from_millis(100));
-    });
-
-    SKey.bind(move || {
-        println!("S key has been pressed");
-        sleep(Duration::from_millis(100));
+        let _ = COMMAND.set([PI, 0.0, 0.0]);
+        sleep(Duration::from_millis(SLEEP.into()));
     });
 
     DKey.bind(move || {
-        println!("D key has been pressed");
-        sleep(Duration::from_millis(100));
+        let _ = COMMAND.set([0.0, 0.0, 0.0]);
+        sleep(Duration::from_millis(SLEEP.into()));
+    });
+
+    ZKey.bind(move || {
+        let _ = COMMAND.set([(5.0*PI/4.0), 0.0, 0.0]);
+        sleep(Duration::from_millis(SLEEP.into()));
+    });
+
+    XKey.bind(move || {
+        let _ = COMMAND.set([(3.0*PI/2.0), 0.0, 0.0]);
+        sleep(Duration::from_millis(SLEEP.into()));
+    });
+
+    CKey.bind(move || {
+        let _ = COMMAND.set([(7.0*PI/4.0), 0.0, 0.0]);
+        sleep(Duration::from_millis(SLEEP.into()));
     });
 
     inputbot::handle_input_events();
@@ -77,8 +92,13 @@ pub fn start_teleoperation() {
 // Get the user input in the form: [dir, speed_scalar, angle_scalar]
 pub fn get_user_input() -> [f32; 3] {
     // TODO: Should it return NONE when not init? Force the user to take care of it ?
-    match COMMAND.clone().into_inner() {
-        Some(x) => return x,
-        None => return [0.0, 0.0, 0.0]
+    let mut result: [f32; 3] = [0.0; 3];
+    println!("{:?}", COMMAND.get().clone());
+    match COMMAND.take() {
+        Some(x) => result = x,
+        None => result = [0.0, 0.0, 0.0]
     }
+    // let _ = COMMAND.set([0.0, 0.0, 0.0]);
+    let _ = COMMAND = OnceLock::new();
+    return result
 }
--- tests/teleop_keyboard_test/src/main.rs
+++ tests/teleop_keyboard_test/src/main.rs
@@ -14,7 +14,7 @@ fn main() {
         println!("Still listening for stuff here {:?} over {:?}", _i, 100);
         // Fetch the data
         let user_input: [f32; 3] = teleop_keyboard::get_user_input();
-        println!("Value returned is {:?}", user_input);
+        // println!("Value returned is {:?}", user_input);
         sleep(Duration::from_millis(100));
     }
 
