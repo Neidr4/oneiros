2b13b7b207ad1590789af06859752771df468dcd
Neidr4 <a.drapbiz@gmail.com>
2024-01-04 22:34:25 +0000

    feat: Wrapped the command into oncecell for keyboard teleop

--- teleop_keyboard/src/lib.rs
+++ teleop_keyboard/src/lib.rs
@@ -1,11 +1,18 @@
+use std::sync::{Mutex, Arc};
+use once_cell::sync::OnceCell;
 use std::f32::consts::PI;
-use std::sync::OnceLock;
 use std::thread;
 use std::{io, thread::sleep, time::Duration};
 use inputbot::KeybdKey::*;
 
-static COMMAND: OnceLock<[f32; 3]> = OnceLock::new();
+static COMMAND_WRAPPER: OnceCell<Command> = OnceCell::new();
 const SLEEP: u8 = 1;
+
+struct Command {
+    command_wrapper: Arc<Mutex<[f32; 3]>>,
+}
+
+// TODO: Add impl for new
 
 pub fn usage() {
     println!(" ------------------------- ");
@@ -35,43 +42,67 @@ fn teleoperate() {
     // TODO: Add ESC for stop
     // TODO: Set the sleep duration as a constant
     QKey.bind(move || {
-        let _ = COMMAND.set([(3.0*PI/4.0), 0.0, 0.0]);
+        match COMMAND_WRAPPER.get() {
+            Some(x) => x.command_wrapper.lock().unwrap().clone_from(&[(3.0*PI/4.0), 0.0, 0.0]),
+            None => println!("The wrapper is not initialized"),
+        }
         sleep(Duration::from_millis(SLEEP.into()));
     });
 
     WKey.bind(move || {
         println!("W key has been pressed");
-        let _ = COMMAND.set([PI/2.0, 0.0, 0.0]);
+        match COMMAND_WRAPPER.get() {
+            Some(x) => x.command_wrapper.lock().unwrap().clone_from(&[PI/2.0, 0.0, 0.0]),
+            None => println!("The wrapper is not initialized"),
+        }
         sleep(Duration::from_millis(SLEEP.into()));
     });
 
     EKey.bind(move || {
-        let _ = COMMAND.set([(3.0*PI/4.0), 0.0, 0.0]);
+        match COMMAND_WRAPPER.get() {
+            Some(x) => x.command_wrapper.lock().unwrap().clone_from(&[(3.0*PI/4.0), 0.0, 0.0]),
+            None => println!("The wrapper is not initialized"),
+        }
         sleep(Duration::from_millis(SLEEP.into()));
     });
 
     AKey.bind(move || {
-        let _ = COMMAND.set([PI, 0.0, 0.0]);
+        match COMMAND_WRAPPER.get() {
+            Some(x) => x.command_wrapper.lock().unwrap().clone_from(&[PI, 0.0, 0.0]),
+            None => println!("The wrapper is not initialized"),
+        }
         sleep(Duration::from_millis(SLEEP.into()));
     });
 
     DKey.bind(move || {
-        let _ = COMMAND.set([0.0, 0.0, 0.0]);
+        match COMMAND_WRAPPER.get() {
+            Some(x) => x.command_wrapper.lock().unwrap().clone_from(&[0.0, 0.0, 0.0]),
+            None => println!("The wrapper is not initialized"),
+        }
         sleep(Duration::from_millis(SLEEP.into()));
     });
 
     ZKey.bind(move || {
-        let _ = COMMAND.set([(5.0*PI/4.0), 0.0, 0.0]);
+        match COMMAND_WRAPPER.get() {
+            Some(x) => x.command_wrapper.lock().unwrap().clone_from(&[(5.0*PI/4.0), 0.0, 0.0]),
+            None => println!("The wrapper is not initialized"),
+        }
         sleep(Duration::from_millis(SLEEP.into()));
     });
 
     XKey.bind(move || {
-        let _ = COMMAND.set([(3.0*PI/2.0), 0.0, 0.0]);
+        match COMMAND_WRAPPER.get() {
+            Some(x) => x.command_wrapper.lock().unwrap().clone_from(&[(3.0*PI/2.0), 0.0, 0.0]),
+            None => println!("The wrapper is not initialized"),
+        }
         sleep(Duration::from_millis(SLEEP.into()));
     });
 
     CKey.bind(move || {
-        let _ = COMMAND.set([(7.0*PI/4.0), 0.0, 0.0]);
+        match COMMAND_WRAPPER.get() {
+            Some(x) => x.command_wrapper.lock().unwrap().clone_from(&[(7.0*PI/4.0), 0.0, 0.0]),
+            None => println!("The wrapper is not initialized"),
+        }
         sleep(Duration::from_millis(SLEEP.into()));
     });
 
@@ -80,6 +111,7 @@ fn teleoperate() {
 }
 
 pub fn start_teleoperation() {
+    //  TODO: Add init of OnceCell here
     thread::spawn(move || {
         println!("Starting the teleoperation keyboard thread");
         teleoperate();
@@ -93,12 +125,11 @@ pub fn start_teleoperation() {
 pub fn get_user_input() -> [f32; 3] {
     // TODO: Should it return NONE when not init? Force the user to take care of it ?
     let mut result: [f32; 3] = [0.0; 3];
-    println!("{:?}", COMMAND.get().clone());
-    match COMMAND.take() {
-        Some(x) => result = x,
+    match COMMAND_WRAPPER.get() {
+        Some(x) => result = x.command_wrapper.lock().unwrap().clone(),
         None => result = [0.0, 0.0, 0.0]
     }
+    println!{
     // let _ = COMMAND.set([0.0, 0.0, 0.0]);
-    let _ = COMMAND = OnceLock::new();
     return result
 }
