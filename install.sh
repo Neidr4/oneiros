7f54fffc26cb8092ac2fdc811ef8bac905ee72e8
Neidr4 <adrien@eatcala.com>
2024-02-02 14:31:02 +0100

    feat: Add draft of the install script

---
+++ install.sh
@@ -0,0 +1,10 @@
+# OS: RaspOS 64 Lite
+
+# config.txt
+# Don't hesite to remove stuff from config.txt (not everything though)
+# Modify /boot/config.txt to add:
+# dtoverlay=pwm-2chan,pin=12,func=4,pin2=13,func2=4
+
+# inputbot
+sudo apt install -y libx11-dev libxtst-dev libudev-dev libinput-dev
+
