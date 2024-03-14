bb76f1a2e67e2d4fd70eaecf2db9dae3f7f7aa59
Neidr4 <adrien@eatcala.com>
2024-03-14 10:03:42 +0100

    feat: Change SystemTime for Instant for sampling rate limiter time

--- control_rate_limiter/src/lib.rs
+++ control_rate_limiter/src/lib.rs
@@ -1,38 +1,34 @@
 use once_cell::sync::OnceCell;
 use std::sync::{Mutex, Arc};
-use std::time::{Duration};
-use std::time::SystemTime;
+use std::time::{Duration, Instant};
 
 const ACCEL_RATE: f32 = 0.01;
 const DECEL_RATE: f32 = -0.01;
-const SAMPLE_TIME: f32 = 0.01;
 static RATE_LIMITER: OnceCell<RateLimiter> = OnceCell::new();
 
 struct RateLimiter {
     motors_speed_previous: Arc<Mutex<[f32; 3]>>,
-    time_previous: Arc<Mutex<SystemTime>>,
+    time_previous: Arc<Mutex<Instant>>,
 }
 
 impl RateLimiter {
     pub fn new() -> Self {
         Self {
             motors_speed_previous: Arc::new(Mutex::new([0.0; 3])),
-            time_previous: Arc::new(Mutex::new(SystemTime::now()))
+            time_previous: Arc::new(Mutex::new(Instant::now()))
         }
     }
 }
 
 pub fn check_rate(motors_speed: &mut [f32; 3]) -> [f32; 3] {
-    let now: SystemTime = SystemTime::now();
+    let now: Instant = Instant::now();
     let obj = RATE_LIMITER.get_or_init(|| RateLimiter::new());
-    let sample_time: Duration = now.duration_since(obj.time_previous.lock().unwrap().clone())
-                                                   .expect("Could not compute time difference");
+    let sample_time: Duration = now.duration_since(obj.time_previous.lock().unwrap().clone());
     for (index, speed) in motors_speed.iter_mut().enumerate() {
         let previous_speed: f32 = *obj.motors_speed_previous.lock().unwrap()
                                       .get(index).expect("Could not grab previous value");
         let rate: f32 = (*speed - previous_speed) / sample_time.as_secs_f32();
-
-        let mut rate_max: f32 = 0.0;
+        let rate_max: f32;
         if rate.is_sign_positive() {
             rate_max = ACCEL_RATE;
         } else {
@@ -43,5 +39,6 @@ pub fn check_rate(motors_speed: &mut [f32; 3]) -> [f32; 3] {
         }
     }
     obj.motors_speed_previous.lock().unwrap().clone_from(motors_speed);
-    return *motors_speed
+    obj.time_previous.lock().unwrap().clone_from(&mut Instant::now());
+    *motors_speed
 }
