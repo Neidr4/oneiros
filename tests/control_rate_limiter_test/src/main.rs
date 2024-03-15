def8d7264e49233c04752114e64d8f6d77976bae
Neidr4 <adrien@eatcala.com>
2024-03-15 04:15:13 +0100

    feat: Change test for the rate limiter

--- tests/control_rate_limiter_test/src/main.rs
+++ tests/control_rate_limiter_test/src/main.rs
@@ -10,6 +10,7 @@ fn main() {
     let mut i : f32 = 0.0;
     let mut variable: [f32; 3] = [0.0; 3];
     let starting_time : Instant = Instant::now();
+
     loop {
         if starting_time.elapsed() > TEST_TIME {
             break;
@@ -18,11 +19,21 @@ fn main() {
         println!("------ i : {:?} --------", i);
         i += 0.01;
         variable.fill(-0.5);
-        // if i < 0.5 {
-        //     variable.fill(0.5);
-        // } else if i > 0.5 && i < 1.0 {
-        //     variable.fill(0.5 - i);
-        // }
+        println!("variable asked   is {:?}", variable);
+        control_rate_limiter::check_rate(&mut variable);
+        println!("variable after   is {:?}", variable);
+        sleep(REFRESH_PERIOD);
+    }
+
+    let starting_time : Instant = Instant::now();
+    loop {
+        if starting_time.elapsed() > TEST_TIME {
+            break;
+        }
+
+        println!("------ i : {:?} --------", i);
+        i += 0.01;
+        variable.fill(0.5);
         println!("variable asked   is {:?}", variable);
         control_rate_limiter::check_rate(&mut variable);
         println!("variable after   is {:?}", variable);
